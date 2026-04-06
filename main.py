b25c3b1d17106830363d10f07fd2a7b454bfa25c
DanielCeaserFBlovaaa <bramvanleijden@gmail.com>
2026-04-06 14:54:40 +0200

    6th Commit, 1 hour session

--- main.py
+++ main.py
@@ -21,17 +21,28 @@ def save_data():
         }, f, indent=4)
 
 #-------------------------------------------#
-#Functions for participants, teams, events, and results 
+#Functions for participants, teams, events, and results and home page
+def show_home():
+    clear_frame(main_frame)
+
+    tk.Label(main_frame, text="Tournament System", font=("Arial", 20)).pack(pady=20)
+
+    tk.Button(main_frame, text="Add Participant", command=add_participant_page).pack(side="left", padx=5)
+    tk.Button(main_frame, text="Add Team", command=add_team_page).pack(side="left", padx=5)
+    tk.Button(main_frame, text="Create Event", command=create_event_page).pack(side="left", padx=5)
+    tk.Button(main_frame, text="Enter Results", command=enter_results_page).pack(side="left", padx=5)
+    tk.Button(main_frame, text="Exit", command=root.quit).pack(side="right", padx=5)
+
 
 def add_participant():
     name_participant = input("Enter your name: ")
     age_participant = input("Enter your age: ")
     ID_participant = participant_ID()
 
-    participants_list.append[{
+    participants_list.append({
         'name': name_participant,
         'age': age_participant,
-        'ID': ID_participant}]
+        'ID': ID_participant})
     save_data() #this will save the data to the JSON file after a participant (but also for the other functions) is added, this ensures that the data is not lost when the app is closed
     
 def add_team():
@@ -39,10 +50,10 @@ def add_team():
     members_team = input("Enter the names of team members (comma separated): ")
     ID_team = team_ID()
 
-    teams_list.append[{
+    teams_list.append({
         'name': name_team,
         'members': members_team,
-        'ID': ID_team}]
+        'ID': ID_team})
     save_data() 
 
 def create_event():
@@ -51,11 +62,11 @@ def create_event():
     event_category = input("Is the event based on a sport or acedemic competition? ")
     ID_event = event_ID()
 
-    events_list.append[{
+    events_list.append({
         'name': name_event,
         'type': event_type,
         'category': event_category,
-        'ID': ID_event}]
+        'ID': ID_event})
     save_data()
     
 def enter_results():
@@ -63,10 +74,10 @@ def enter_results():
     participant_name = input("Enter the name of the participant/team: ") 
     position = int(input("Enter the final position (1-5): ")) 
     
-    results_list.append[{
+    results_list.append({
         'event': event_name, 
         'participant': participant_name, 
-        'position': position}]
+        'position': position})
     save_data() 
     
 def participant_ID():
@@ -88,9 +99,10 @@ root.geometry("1000x700")
 
 #-----------------------------#
 #Main Frame 
-def main_frame():
-    clear_frame(root)
-    tk.Label(main_frame, text="Tournament System", font=("Arial", 20)).pack(pady=20)
+
+main_frame = tk.Frame(root)
+main_frame.pack(pady=20)
+tk.Label(main_frame, text="Tournament System", font=("Arial", 20)).pack(pady=20)
     
     
 #Functions 
@@ -101,8 +113,9 @@ def onclick(removal):
 
 #Add Participant page
 def add_participant_page():
-    clear_frame(root)
-    tk.Label(root, text="Add a Participant", font=("Arial", 20)).pack(pady=20)
+    clear_frame(main_frame)
+    tk.Label(main_frame, text="Add a Participant", font=("Arial", 20)).pack(pady=20)
+    tk.Label(main_frame, text="Please remember your participant ID, as you will need it for more.").pack(pady=5)
    
     entry_name = tk.Entry(main_frame)
     entry_name.pack(pady=10)
@@ -120,29 +133,72 @@ def add_participant_page():
 
     def submit():
         pid=participant_ID()
+        
 
         participants_list.append({
             'name': entry_name.get(),
             'age': entry_age.get(),
             'ID': pid})
         save_data() #this will save the data to the JSON file after a participant is added
-        result_laber.config(text=f"Participant added with ID: {pid}") #this will show the user that the participant has been added and display their ID
+        tk.Label(main_frame, text=f"Your participant ID is: {pid}, you have successfully entered the tournament!").pack(pady=5) #this will show the user their participant ID after they have clicked submit, this is important as they will need it for more
+        tk.Button(main_frame, text="Return to Home", command=show_home).pack(pady=5) #this will return the user to the home page after they have added a participant
+
+
     submit_button = tk.Button(main_frame, text="Submit", command=submit)
     submit_button.pack(pady=5)
+    
 #Add Team page
 def add_team_page():
-    clear_frame(root) 
-    open 
+    clear_frame(main_frame)
+    tk.Label(main_frame, text="Add a Team", font=("Arial", 20)).pack(pady=20)
+    tk.Label(main_frame, text="Please remember your team ID, as you will need it for more.").pack(pady=5)
+
+    entry_name = tk.Entry(main_frame)
+    entry_name.pack(pady=10)
+    entry_name.insert(0, "Enter your team name")
+    entry_name.bind("<FocusIn>", onclick) #this will clear the entry box when the user clicks on it, for better structure and a more professional app
+    
+    entry_contactperson = tk.Entry(main_frame)
+    entry_contactperson.pack()
+    entry_contactperson.pack(pady=10)
+    entry_contactperson.insert(0, "Enter the name of the contact person for the team")
+    entry_contactperson.bind("<FocusIn>", onclick) 
+
+    entry_contactdetails = tk.Entry(main_frame)
+    entry_contactdetails.pack()
+    entry_contactdetails.pack(pady=10)
+    entry_contactdetails.insert(0, "Enter the email address of the contact person for the team")
+    entry_contactdetails.bind("<FocusIn>", onclick)
+
+    result_laber = tk.Label(main_frame, text="")
+    result_laber.pack(pady=5)
+
+    def submit():
+        tid=team_ID()
+        
+
+        teams_list.append({
+            'teamname': entry_name.get(),
+            'contactperson': entry_contactperson.get(),
+            'contactdetails': entry_contactdetails.get(),
+            'teamID': tid})
+        save_data() #this will save the data to the JSON file after a team is added
+        tk.Label(main_frame, text=f"Your team ID is: {tid}, you have successfully entered the tournament!").pack(pady=5) #this will show the user their team ID after they have clicked submit, this is important as they will need it for more
+        tk.Button(main_frame, text="Return to Home", command=show_home).pack(pady=5) #this will return the user to the home page after they have added a team
+
+
+    submit_button = tk.Button(main_frame, text="Submit", command=submit)
+    submit_button.pack(pady=5)
 
 
 #Create Event page
 def create_event_page():
-    clear_frame(root)
+    clear_frame(main_frame)
     open
 
 #Enter Results page
 def enter_results_page():
-    clear_frame(root)
+    clear_frame(main_frame)
     open
 
 
@@ -156,15 +212,6 @@ def clear_frame(frame):
 #------------------------------#
 #Menu Buttons 
 
-tk.Label(root, text="Welcome to the Tournament Scoring System", font=("Arial", 20)).pack(pady=20) #this is the title of the app, it will be displayed at the top of the main menu
-
-tk.Button(main_frame, text="Add Participant", command=add_participant_page).pack(side="left", padx=5)
-tk.Button(main_frame, text="Add Team", command=add_team_page).pack(side="left", padx=5)
-tk.Button(main_frame, text="Create Event", command=create_event_page).pack(side="left", padx=5)
-tk.Button(main_frame, text="Enter Results", command=enter_results_page).pack(side="left", padx=5)
-tk.Button(main_frame, text="Exit", command=root.quit).pack(side="right", padx=5)
-
-#Start of the app
-main_frame() #this will show the main menu when the app is started
+show_home()  # home page start 
 
 root.mainloop() 
